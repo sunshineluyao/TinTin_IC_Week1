e0938c4d52174494251d5af84555c45e7c8c57f7
duke-sunshine <lz183@duke.edu>
2022-02-21 21:45:57 +0800

    Initial commit.

---
+++ src/tin_sunshine_assets/src/index.js
@@ -0,0 +1,19 @@
+import { tin_sunshine } from "../../declarations/tin_sunshine";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await tin_sunshine.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
